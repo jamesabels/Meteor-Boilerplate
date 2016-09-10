8ea308f0d31aad19c90eab1808af3c6388ff212b
James Abels <jamesrabels@gmail.com>
2016-09-10 18:45:06 -0400

    Install Semantic

--- client/main.jsx
+++ client/main.jsx
@@ -5,17 +5,17 @@ import { mount } from 'react-mounter'
 // Import Components
 import Hello from './imports/containers/hello.jsx';
 
-// Import Layouts 
+// Import Layouts
 import MainLayout from './imports/main-layout.jsx';
 
 // Import  Middleware
 import thunk from 'redux-thunk';
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
- 
+
 // Import react redux
 import { createStore, applyMiddleware } from 'redux';
 
-// Import Reducer 
+// Import Reducer
 import rootReducer from './imports/reducers/root_reducer.jsx';
 
 FlowRouter.route("/", {
