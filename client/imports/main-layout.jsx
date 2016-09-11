c383cc6f5bb6b495bc3e6e0d32cba96399d8d08d
James Abels <jamesrabels@gmail.com>
2016-09-11 16:26:16 -0400

    Add components for navigation and footer

--- client/imports/main-layout.jsx
+++ client/imports/main-layout.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import  {Provider} from 'react-redux';
 
 
-export default MainLayout = ({sidebar, content, store}) => (
+export default MainLayout = ({sidebar, content, nav, footer, store}) => (
     <Provider store={store}>
-        <div>
-            <div className="sidebar">
-                {sidebar}
+        <div className="page-wrap">
+            <div className="nav-wrap">
+                {nav}
             </div>
-            <div className="content">
-                {content}
+            <div className="content-wrap">
+                <div className="sidebar-wrap">
+                    {sidebar}
+                </div>
+                <div className="content">
+                    {content}
+                </div>
+            </div>
+            <div className="footer-wrap">
+                {footer}
             </div>
         </div>
     </Provider>
